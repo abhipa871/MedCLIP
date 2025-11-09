755007a97c0bb2d71b4527caef90a0374809b799
root <root@abhi.>
2025-11-08 21:50:41 -0500

    altered requirements

--- medclip/dataset.py
+++ medclip/dataset.py
@@ -133,7 +133,7 @@ class MedCLIPFeatureExtractor(CLIPFeatureExtractor):
 
     def pad_img(self, img, min_size=224, fill_color=0):
         '''pad img to square.
-        ''''
+        '''
         print(type(x))
         print(type(y))
         print(x)
