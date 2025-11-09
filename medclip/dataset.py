cfbfa14826579b4d8e5b3b3ee46c88d682476301
root <root@abhi.>
2025-11-08 22:05:31 -0500

    medclip-fix processor

--- medclip/dataset.py
+++ medclip/dataset.py
@@ -134,10 +134,7 @@ class MedCLIPFeatureExtractor(CLIPFeatureExtractor):
     def pad_img(self, img, min_size=224, fill_color=0):
         '''pad img to square.
         '''
-        print(type(x))
-        print(type(y))
-        print(x)
-        print(y)
+
         x, y = img.size
         min_size = max(min_size.values())
         size = max(min_size, x, y)
